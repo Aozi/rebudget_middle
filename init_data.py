a75769fb64477687406cc09bc76246bb1fbe33f6
Aozi <niko.novitsky@gmail.com>
2017-11-25 18:25:41 +0200

    Stuff

---
+++ init_data.py
@@ -0,0 +1,46 @@
+from app import db, User, Transaction
+
+i = user.insert()
+i.execute({id:1}, {id:2}, {id:3})
+
+i2 = transaction.insert()
+i2.execute(
+	{
+	id:0, 
+	user_id:1,
+	amount:100,
+	company:"K Kauppa"
+	},
+
+	{
+	id:1, 
+	user_id:1,
+	amount:20,
+	company:"K Kauppa"
+	},
+
+
+	{
+	id:2, 
+	user_id:1,
+	amount:32,
+	company:"K Kauppa"
+	},
+
+
+	{
+	id:3, 
+	user_id:1,
+	amount:10,
+	company:"K Kauppa"
+	},
+
+	{
+	id:4, 
+	user_id:1,
+	amount:300,
+	company:"K Kauppa"
+
+	}
+
+	)
