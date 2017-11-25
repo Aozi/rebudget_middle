d69a9bee99fe625e33be43e3de78bdda13d80ad7
Aozi <niko.novitsky@gmail.com>
2017-11-25 19:45:37 +0200

    sdfsaf

--- init_data.py
+++ init_data.py
@@ -2,6 +2,6 @@ from app import User, Transaction, db
 
 new_user = User(id=0)
 new_tr = Transaction(id=0,user_id=0,amount=100,company="blah")
+new_user.transactions.add(new_tr)
 db.session.add(new_user)
-db.session.add(new_tr)
 db.session.commit()
