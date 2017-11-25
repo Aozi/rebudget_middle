6a3faad70125ab77b738d5d9dfe794b60f78e428
Aozi <niko.novitsky@gmail.com>
2017-11-25 20:40:06 +0200

    a

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ db = SQLAlchemy(app)
 session = db.sessionmaker(bind=db.get_engine())
 
 # Create our database model
-class User(db.Model):
-    __tablename__ = "user"
+class bank_User(db.Model):
+    __tablename__ = "bank_user"
     u_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     balance = db.Column(db.Float)
-    transactions = db.relationship("Transaction", backref='user', lazy=True)
+    transactions = db.relationship("Transaction", backref='bank_user', lazy=True)
 
     def __init__(self, u_id, name, balance):
         self.u_id = u_id
@@ -25,7 +25,7 @@ class User(db.Model):
 class Transaction(db.Model):
     __tablename__ = 'transaction'
     t_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.u_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('bank_user.u_id'))
     amount = db.Column(db.Float)
     company = db.Column(db.String)
     time = db.Column(db.DateTime)
@@ -34,7 +34,7 @@ db.create_all()
 
 manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
 
-manager.create_api(User, methods=['GET', 'POST', 'PATCH' 'DELETE'], allow_patch_many=True, allow_delete_many=True)
+manager.create_api(bank_User, methods=['GET', 'POST', 'PATCH' 'DELETE'], allow_patch_many=True, allow_delete_many=True)
 manager.create_api(Transaction, methods=['GET', 'POST', 'DELETE'])
 
 
--- init_data.py
+++ init_data.py
@@ -1,6 +1,6 @@
-from app import User, Transaction, db
+from app import bank_User, Transaction, db
 
-new_user = User(u_id=0,name="test",balance=10000)
+new_user = bank_User(u_id=0,name="test",balance=10000)
 new_tr = Transaction(t_id=0,user_id=0,amount=100,company="blah")
 new_user.transactions.append(new_tr)
 db.session.add(new_user)
