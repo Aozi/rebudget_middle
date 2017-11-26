74e9fae37b09906d477fa3023b8bb4ccf412db89
Aozi <niko.novitsky@gmail.com>
2017-11-26 02:20:51 +0200

    com

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,19 +1,25 @@
 import csv
 import json
 import requests
-import pandas as pd
 
-#with open('sdata.csv') as f:
-#    columns = ("date", "amount", "place", "category")
-#    reader = csv.DictReader(f, columns)
-#    rows = list(reader)
-#with open('output.json', 'w') as f:
-#    json.dump(rows, f, indent=4)
+
+with open('transactions.csv') as f:
+    columns = ("date", "amount", "place", "categories")
+    reader = csv.DictReader(f, columns)
+    rows = list(reader)
+with open('output.json', 'w') as f:
+    json.dump(rows, f, indent=4)
+
+
 
 transactions = json.load(open("output.json"))
 
 for trans in transactions:
-    amount_in_float = trans['amount']
-    amount_in_float = amount_in_float.replace(",",".")
-    print amount_in_float
-
+    r = requests.post("https://intense-shore-10684.herokuapp.com/api/transaction", json={
+        'user_id': 1,
+        'amount': trans['amount'],
+        'company': trans['place'],
+        'date': trans['date'],
+        'category': trans['categories']
+    })
+    print(r.status_code, r.reason)
--- init_data.py
+++ init_data.py
@@ -1,8 +1,6 @@
 from app import bank_User, Transaction, db
 
 new_user = bank_User(name="test",balance=10000)
-new_tr = Transaction(user_id=0,amount=100,company="blah",categor="luxury")
 
-new_user.transactions.append(new_tr)
 db.session.add(new_user)
 db.session.commit()
