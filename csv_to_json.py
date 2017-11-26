2a2ca511a63ae86fcddd7e1057a445564b258e68
Aozi <niko.novitsky@gmail.com>
2017-11-26 02:36:07 +0200

    sa

--- csv_to_json.py
+++ csv_to_json.py
@@ -3,23 +3,24 @@ import json
 import requests
 
 
-with open('transactions.csv') as f:
-    columns = ("date", "amount", "place", "categories")
-    reader = csv.DictReader(f, columns)
-    rows = list(reader)
-with open('output.json', 'w') as f:
-    json.dump(rows, f, indent=4)
+#with open('transactions.csv') as f:
+#    columns = ("date", "amount", "place", "categories")
+#    reader = csv.DictReader(f, columns)
+#    rows = list(reader)
+#with open('output.json', 'w') as f:
+#    json.dump(rows, f, indent=4)
 
 
 
 transactions = json.load(open("output.json"))
 
 for trans in transactions:
+
     r = requests.post("https://intense-shore-10684.herokuapp.com/api/transaction", json={
-        'user_id': 1,
+        'user_id': 3,
         'amount': trans['amount'],
         'company': trans['place'],
-        'date': trans['date'],
+        'time': trans['date'],
         'category': trans['categories']
     })
     print(r.status_code, r.reason)
